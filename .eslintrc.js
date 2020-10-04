68efd376d5f6c1d7c9d183885529993cb929d3fe
Daniel Stout <daniel@stoutlabs.com>
2020-10-04 03:24:54 -0400

    fix error in no-unused-vars config

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,7 +38,7 @@ module.exports = {
     "no-unused-vars": [
       1,
       {
-        "ignoreSiblings": true,
+        "ignoreRestSiblings": true,
         "argsIgnorePattern": "res|next|^err"
       }
     ],
